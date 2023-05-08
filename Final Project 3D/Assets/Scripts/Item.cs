cb4abdaffd4bb29e85847d52e7927b76baafd0c4
YinelaPrimero <yinelaprimero01@gmail.com>
2023-05-07 22:39:44 -0500

    Usar pistola de inventario

--- Final Project 3D/Assets/Scripts/Item.cs
+++ Final Project 3D/Assets/Scripts/Item.cs
@@ -11,15 +11,71 @@ public class Item : MonoBehaviour
 
     [HideInInspector] // a pesar de ser una variable publica, no se verá en el inspector 
     public bool coleccionado; 
-    // Start is called before the first frame update
+
+    [HideInInspector]
+    public bool equipado;
+
+    [HideInInspector]
+    public GameObject controladorArmas;
+
+    [HideInInspector]
+    public GameObject arma;
+
+    private int cantidadArmas;
+
+  
+    public GameObject Personaje;
+    public Animator animPersonaje;
+
+    
     void Start()
     {
+        Personaje = GameObject.FindWithTag("Player");
+        animPersonaje = Personaje.GetComponent<Animator>();
+
+        controladorArmas = GameObject.FindWithTag("ControladorArmas");
         
+        int cantidadArmas = controladorArmas.transform.childCount;
+        Debug.Log("Cogi la cantidad de armas" + cantidadArmas.ToString());
+        
+        for(int i = 0; i<cantidadArmas; i++)
+        {
+            if(controladorArmas.transform.GetChild(i).gameObject.GetComponent<Item>().Id==Id)
+            {
+                arma = controladorArmas.transform.GetChild(i).gameObject;
+                Debug.Log("Cogi la referencia de arma");
+            }
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(equipado)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                equipado = false;
+            }
+            if(equipado == false)
+            {
+                gameObject.SetActive(false);
+            }
+
+        }
         
+    }
+
+    public void UsoItem()
+    {
+        if(tipo == "Arma")
+        {
+            arma.SetActive(true);
+            arma.GetComponent<Item>().equipado = true;
+            animPersonaje.SetBool("Armado", true);
+        }
+
     }
 }
--- Final Project 3D/Assets/Scripts/MovCamera3Person.cs
+++ Final Project 3D/Assets/Scripts/MovCamera3Person.cs
@@ -17,7 +17,7 @@ public class MovCamera3Person : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
